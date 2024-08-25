698898f3583f4816d9a9524c5f533c9a8997f7fb
mohammadReza-kaspour <kaspourmohammad@gmail.com>
2024-08-26 02:05:37 +0330

    Add project files.

---
+++ HotelRezervation.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelRezervation.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
